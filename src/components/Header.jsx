a0f9783440c24c9f86455d979939961e794e496a
coder-gaia <alexandregaia911@gmail.com>
2024-12-20 19:27:55 -0300

    Projects & About sections - created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     >
       <Navbar />
       <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
-        <h2 className="text-5x1 sm:text-[36px] md:text-[82px] inline-block max-w-3x1 font-semibold pt-20">
+        <h2 className="text-5xl sm:text-[36px] md:text-[82px] inline-block max-w-3x1 font-semibold pt-20">
           Explore homes that fit your dreams
         </h2>
         <div className="space-x-6 mt-16">
